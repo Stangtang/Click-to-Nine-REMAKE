40e4ce336f4df8c9180aae083a59e2473ce00deb
weewow <temp@email.com>
2026-09-21 00:03:25 -0500

    greatly improve clicking experience

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+#include <algorithm>
+
 using namespace std;
 
 int main(void) {
@@ -17,8 +19,8 @@ int main(void) {
     SetTargetFPS(GetMonitorRefreshRate(monitor));
 
     SearchAndSetResourceDir("resources");
-    const int fontSize = 40;
-    const float textSpacing = 2;
+    constexpr int fontSize = 40;
+    constexpr float textSpacing = 2.0f;
     Font font = LoadFontEx("calibri-regular.ttf", fontSize, nullptr, 0);
 
     char clicks = '0';
@@ -27,6 +29,23 @@ int main(void) {
     Vector2 textPosition = {
         (displayWidth - textSize.x) / 2,
         (displayHeight - textSize.y) / 2
+    };
+
+    const Color numberColor = DARKGRAY;
+    constexpr float opaqueAlpha = 1.0f;
+    constexpr float startingAlpha = opaqueAlpha * 0.5f;
+    constexpr float fadeTimeSeconds = 0.5f;
+    constexpr float fadeSpeed = startingAlpha / fadeTimeSeconds;
+    float alpha = 0;
+
+    constexpr int numberFontSize = 3000;
+    constexpr float numberTextSpacing = 0.0f;
+    char numberText = '0';
+    char numberTextStr[2] = { numberText, '\0' };
+    Vector2 numberTextSize = MeasureTextEx(font, numberTextStr, numberFontSize, numberTextSpacing);
+    Vector2 numberTextPosition = {
+        (displayWidth - numberTextSize.x) / 2,
+        (displayHeight - numberTextSize.y) / 2
     };
 
     while (!WindowShouldClose()) { // Detect window close button or ESC key
@@ -42,7 +61,20 @@ int main(void) {
                 (displayWidth - textSize.x) / 2,
                 (displayHeight - textSize.y) / 2
             };
+
+            numberTextStr[0] = clicks;
+            numberTextSize = MeasureTextEx(font, numberTextStr, numberFontSize, numberTextSpacing);
+            numberTextPosition = {
+                (displayWidth - numberTextSize.x) / 2,
+                (displayHeight - numberTextSize.y) / 2
+            };
+            alpha = startingAlpha;
         }
+
+        if (alpha > 0.0f) {
+            alpha = max(alpha - fadeSpeed * GetFrameTime(), 0.0f);
+        }
+        Color transparentNumberColor = ColorAlpha(numberColor, alpha);
 
         BeginDrawing();
 
@@ -51,6 +83,9 @@ int main(void) {
         // DrawFPS(10, 10);
 
         DrawTextEx(font, clicksStr, textPosition, fontSize, textSpacing, BLACK);
+        if (alpha > 0.0f) {
+            DrawTextEx(font, numberTextStr, numberTextPosition, numberFontSize, numberTextSpacing, transparentNumberColor);
+        }
 
         EndDrawing();
     }
